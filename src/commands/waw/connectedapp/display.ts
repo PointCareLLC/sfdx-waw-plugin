5ac279c5dc8cb4523a0f9c29491b7be8c3136e6f
Wade Wegner <wade.wegner@gmail.com>
2018-06-17 09:51:31 -0700

    Added create command and refactored

--- src/commands/waw/connectedapp/display.ts
+++ src/commands/waw/connectedapp/display.ts
@@ -2,7 +2,7 @@ import {core, SfdxCommand, flags} from '@salesforce/command';
 
 export default class Display extends SfdxCommand {
 
-  public static description = 'List the connected apps in your org';
+  public static description = 'Display the details of a connected app in your org';
 
   protected static flagsConfig = {
     connectedappname: flags.string(
@@ -12,21 +12,14 @@ export default class Display extends SfdxCommand {
       })
   };
 
-  // Comment this out if your command does not require an org username
+  // sfdx options
   protected static requiresUsername = true;
-
-  // Comment this out if your command does not support a hub org username
   protected static supportsDevhubUsername = false;
-
-  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = false;
 
   public async run(): Promise<any> { // tslint:disable-line:no-any
     
     const connectedAppName = this.flags.connectedappname;
-
-    // refresh auth
-    await this.org.refreshAuth();
 
     this.org.getConnection().metadata.read('ConnectedApp', connectedAppName, (readErr, metadataResult) => {
       if (readErr) {
--- src/commands/waw/connectedapp/list.ts
+++ src/commands/waw/connectedapp/list.ts
@@ -4,20 +4,13 @@ export default class List extends SfdxCommand {
 
   public static description = 'List the connected apps in your org';
 
-  // Comment this out if your command does not require an org username
+  // sfdx options
   protected static requiresUsername = true;
-
-  // Comment this out if your command does not support a hub org username
   protected static supportsDevhubUsername = false;
-
-  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = false;
 
   public async run(): Promise<any> { // tslint:disable-line:no-any
     
-    // refresh auth
-    await this.org.refreshAuth();
-
     var types = [{type: 'ConnectedApp', folder: null}];
     this.org.getConnection().metadata.list(types, '42.0', (readErr, metadataResult) => {
       if (readErr) {
